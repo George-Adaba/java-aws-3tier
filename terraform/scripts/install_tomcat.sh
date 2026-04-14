958e72b969dba9ab504688e4763aa8d14c56f5b3
George-Adaba <adaba.george001@gmail.com>
2026-04-14 17:18:48 +0100

    fixed pipeline bugs

--- terraform/scripts/install_tomcat.sh
+++ terraform/scripts/install_tomcat.sh
@@ -12,3 +12,12 @@ sudo systemctl start tomcat10
 
 # Placeholder for war deployment
 # You can use aws s3 cp or scp to copy the artifact to /var/lib/tomcat/webapps/
+
+# Ensure the directory exists
+sudo mkdir -p /opt/tomcat/webapps/
+
+# Change ownership so ec2-user can upload files there
+sudo chown -R ec2-user:ec2-user /opt/tomcat/webapps/
+
+# Optional: Give it write permissions just to be safe
+sudo chmod -R 755 /opt/tomcat/webapps/
